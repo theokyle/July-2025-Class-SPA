5ed338b7cc8bb1632390157707afce0b919e8353
theokyle <theokyle@gmail.com>
2025-09-16 19:27:51 -0500

    connect order form to backend api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {header, nav, main, footer} from "./components";
+import { header, nav, main, footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { camelCase } from "lodash";
@@ -7,7 +7,7 @@ import axios from "axios";
 const router = new Navigo("/");
 
 function render(state = store.home) {
-  document.querySelector('#root').innerHTML = `
+  document.querySelector("#root").innerHTML = `
   ${header(state)}
   ${nav(store.nav)}
   ${main(state)}
@@ -26,28 +26,28 @@ router.hooks({
       // Add a case for each view that needs data from an API
       case "home":
         axios
-        .get(
-      `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=imperial&q=st%20louis`
-      )
-        .then(response => {
-          store.home.weather = {
-            city: response.data.name,
-            temp: response.data.main.temp,
-            feelsLike: response.data.main.feels_like,
-            description: response.data.weather[0].main
-          };
-          done();
-        })
-        .catch((err) => {
-          console.log(`failed to fetch weather data: ${err}`);
-          done();
-        })
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=imperial&q=st%20louis`
+          )
+          .then((response) => {
+            store.home.weather = {
+              city: response.data.name,
+              temp: response.data.main.temp,
+              feelsLike: response.data.main.feels_like,
+              description: response.data.weather[0].main,
+            };
+            done();
+          })
+          .catch((err) => {
+            console.log(`failed to fetch weather data: ${err}`);
+            done();
+          });
         break;
       case "pizza":
         // New Axios get request utilizing already made environment variable
         axios
-          .get(`https://sc-pizza-api.onrender.com/pizzas`)
-          .then(response => {
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .then((response) => {
             // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
             store.pizza.pizzas = response.data;
             done();
@@ -56,11 +56,11 @@ router.hooks({
             console.log("It puked", error);
             done();
           });
-          break;
-      default :
+        break;
+      default:
         // We must call done for all views so we include default for the views that don't have cases above.
         done();
-        // break is not needed since it is the last condition, if you move default higher in the stack then you should add the break statement.
+      // break is not needed since it is the last condition, if you move default higher in the stack then you should add the break statement.
     }
   },
   already: (match) => {
@@ -71,24 +71,72 @@ router.hooks({
   after: (match) => {
     router.updatePageLinks();
 
-    // add menu toggle to bars icon in nav bar
-    document.querySelector(".fa-bars").addEventListener("click", () => {
-        document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-    });
-  }
-});
-
-router.on({
-  "/": () => render(),
-  
-  '/:view': function(match) {
     const view = match?.data?.view ? camelCase(match.data.view) : "home";
 
-    if (view in store) {
-      render(store[view]);
-    } else {
-      render(store.viewNotFound);
-      console.log(`View ${view} not defined`);
+    if (view === "order") {
+      // Add an event handler for the submit button on the form
+      document.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        // Get the form element
+        const inputList = event.target.elements;
+
+        // Create an empty array to hold the toppings
+        const toppings = [];
+
+        // Iterate over the toppings array
+        console.log(inputList.toppings.value);
+        for (let input of inputList.toppings) {
+          // If the value of the checked attribute is true then add the value to the toppings array
+          if (input.checked) {
+            toppings.push(input.value);
+          }
+        }
+
+        // Create a request body object to send to the API
+        const requestData = {
+          customer: inputList.customer.value,
+          crust: inputList.crust.value,
+          cheese: inputList.cheese.value,
+          sauce: inputList.sauce.value,
+          toppings: toppings,
+        };
+
+        axios
+          // Make a POST request to the API to create a new pizza
+          .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData)
+          .then((response) => {
+            //  Then push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+            store.pizza.pizzas.push(response.data);
+            router.navigate("/pizza");
+          })
+          // If there is an error log it to the console
+          .catch((error) => {
+            console.log("It puked", error);
+          });
+      });
     }
-  }
-}).resolve();
+
+    // add menu toggle to bars icon in nav bar
+    document.querySelector(".fa-bars").addEventListener("click", () => {
+      document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+    });
+  },
+});
+
+router
+  .on({
+    "/": () => render(),
+
+    "/:view": function(match) {
+      const view = match?.data?.view ? camelCase(match.data.view) : "home";
+
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.viewNotFound);
+        console.log(`View ${view} not defined`);
+      }
+    },
+  })
+  .resolve();
--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import pizzas from "./controllers/pizzas.js";
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 mongoose.connect(process.env.MONGODB);
 const db = mongoose.connection;
